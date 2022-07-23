77e7f2824d0185bc7db598ffc9341ffb6f411caa
Prathamesh Kumbhar <prathameshkumbhar291113@gmail.com>
2022-07-23 22:28:13 +0530

    B Fit

---
+++ app/src/main/java/com/example/bfit/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.bfit
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import splitties.activities.start
+import kotlin.concurrent.thread
+import kotlin.coroutines.suspendCoroutine
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+            start<HomeActivity>(){
+                finish()
+            }
+        },2000)
+
+    }
+
+}
