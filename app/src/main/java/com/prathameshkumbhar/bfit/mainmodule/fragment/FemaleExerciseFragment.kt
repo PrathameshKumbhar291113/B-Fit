a8af3297ef5ed85c0e95e3f66753283fda04e67c
Prathamesh Kumbhar <99268055+PrathameshKumbhar291113@users.noreply.github.com>
2022-08-31 16:37:43 +0530

    [Code Changes]: 1. Integrated the third party library in build.gradle (Meow Custom Navigation Bar) . 2. Added the MaleExerciseFragment.kt and FemaleExerciseFragment.kt for particular gender . 3. Added the icons for the bottom navigation bar.

--- app/src/main/java/com/prathameshkumbhar/bfit/mainmodule/fragment/FemaleExerciseFragment.kt
+++ app/src/main/java/com/prathameshkumbhar/bfit/mainmodule/fragment/FemaleExerciseFragment.kt
@@ -7,14 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.prathameshkumbhar.bfit.R
 
-class HomeFragment : Fragment() {
+class FemaleExerciseFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_female_exercise, container, false)
     }
-
 }
--- app/src/main/java/com/prathameshkumbhar/bfit/mainmodule/fragment/MaleExerciseFragment.kt
+++ app/src/main/java/com/prathameshkumbhar/bfit/mainmodule/fragment/MaleExerciseFragment.kt
@@ -7,15 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.prathameshkumbhar.bfit.R
 
-
-class SettingsFragment : Fragment() {
+class MaleExerciseFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.fragment_male_exercise, container, false)
     }
 
 }
