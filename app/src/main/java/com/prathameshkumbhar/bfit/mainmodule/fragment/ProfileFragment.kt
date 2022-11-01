acf808f7e5aed2cc11ccf0e4baea260668e80371
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2022-11-02 00:23:40 +0530

    [Bug Fixes]: Made all the values false stored in shared preferences inside the ProfileFragment.kt which is default value, then afterwards we can re write its value to true / false on our choice. Hence this will help in re write the data when login again in . [Code Changes]: Design the fragment_profile.xml again.

--- app/src/main/java/com/prathameshkumbhar/bfit/mainmodule/fragment/ProfileFragment.kt
+++ app/src/main/java/com/prathameshkumbhar/bfit/mainmodule/fragment/ProfileFragment.kt
@@ -22,7 +22,6 @@ class ProfileFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var firebaseAuth : FirebaseAuth
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -39,39 +38,13 @@ class ProfileFragment : Fragment() {
 
         binding.logoutButton.setOnClickListener {
 
-            //putting value 'false' in the shared preference as the user is route to the splash
-            //activity, they will again route to the login fragment as the value now stored is
-            //false as if the value stored in the logged in is false then the user will have to
-            //log in again
-
-            val sharePrefLogin : SharedPreferences = context!!.getSharedPreferences("login", Context.MODE_PRIVATE)
-            var editorlog : SharedPreferences.Editor = sharePrefLogin.edit()
-            editorlog.putBoolean("flag",false)
-            editorlog.apply()
-
-            //Same goes for the on board activity fragment also
-            val sharePrefOnboarded : SharedPreferences = context!!.getSharedPreferences("onBoardCheck", Context.MODE_PRIVATE)
-            var editor: SharedPreferences.Editor = sharePrefOnboarded.edit()
-            editor.putBoolean("isOnboardComplete",false)
-            editor.apply()
-            
-            //Changing the already selected level of exercise to default false
-            val sharePrefBeginner : SharedPreferences = context!!.getSharedPreferences("cardBeginner", Context.MODE_PRIVATE)
-            var beginnerEditor : SharedPreferences.Editor = sharePrefBeginner.edit()
-            beginnerEditor.putBoolean("isBeginnerCardCheck", false)
-            beginnerEditor.apply()
-
-
-            val sharePrefIntermediate : SharedPreferences = context!!.getSharedPreferences("cardIntermediate", Context.MODE_PRIVATE)
-            var intermediateEditor : SharedPreferences.Editor = sharePrefIntermediate.edit()
-            intermediateEditor.putBoolean("isIntermediateCardCheck", false)
-            intermediateEditor.apply()
-
-
-            val sharePrefAdvance : SharedPreferences = context!!.getSharedPreferences("cardAdvance", Context.MODE_PRIVATE)
-            var advanceEditor : SharedPreferences.Editor = sharePrefAdvance.edit()
-            advanceEditor.putBoolean("isAdvanceCardCheck", false)
-            advanceEditor.apply()
+            //Making the all shared preferences value to false again to make it set for the default
+            //value
+            isLoggedIn()
+            isOnBoarded()
+            resetLevelOfExercise()
+            resetBodyTypeGoal()
+            resetDietType()
 
 
             requireContext().showSuccessToast("Successfully Logged Out!")
@@ -83,5 +56,105 @@ class ProfileFragment : Fragment() {
                 }
             }
         }
+    }
+
+
+    private fun isLoggedIn(){
+        //putting value 'false' in the shared preference as the user is route to the splash
+        //activity, they will again route to the login fragment as the value now stored is
+        //false as if the value stored in the logged in is false then the user will have to
+        //log in again
+
+        val sharePrefLogin : SharedPreferences = context!!.getSharedPreferences("login", Context.MODE_PRIVATE)
+        var editorlog : SharedPreferences.Editor = sharePrefLogin.edit()
+        editorlog.putBoolean("flag",false)
+        editorlog.apply()
+
+    }
+
+    private fun isOnBoarded(){
+        //Same goes for the on board activity fragment also
+        val sharePrefOnboarded : SharedPreferences = context!!.getSharedPreferences("onBoardCheck", Context.MODE_PRIVATE)
+        var editor: SharedPreferences.Editor = sharePrefOnboarded.edit()
+        editor.putBoolean("isOnboardComplete",false)
+        editor.apply()
+
+    }
+
+    private fun resetLevelOfExercise(){
+        //Changing the already selected level of exercise to default false
+        val sharePrefBeginner : SharedPreferences = context!!.getSharedPreferences("cardBeginner", Context.MODE_PRIVATE)
+        var beginnerEditor : SharedPreferences.Editor = sharePrefBeginner.edit()
+        beginnerEditor.putBoolean("isBeginnerCardCheck", false)
+        beginnerEditor.apply()
+
+
+        val sharePrefIntermediate : SharedPreferences = context!!.getSharedPreferences("cardIntermediate", Context.MODE_PRIVATE)
+        var intermediateEditor : SharedPreferences.Editor = sharePrefIntermediate.edit()
+        intermediateEditor.putBoolean("isIntermediateCardCheck", false)
+        intermediateEditor.apply()
+
+
+        val sharePrefAdvance : SharedPreferences = context!!.getSharedPreferences("cardAdvance", Context.MODE_PRIVATE)
+        var advanceEditor : SharedPreferences.Editor = sharePrefAdvance.edit()
+        advanceEditor.putBoolean("isAdvanceCardCheck", false)
+        advanceEditor.apply()
+
+    }
+
+    private fun resetBodyTypeGoal(){
+        val sharePrefLooseWeight : SharedPreferences = context!!.getSharedPreferences(
+            "cardLooseWeightChecked",
+            Context.MODE_PRIVATE
+        )
+        var looseWeightEditor : SharedPreferences.Editor = sharePrefLooseWeight.edit()
+        looseWeightEditor.putBoolean("isLooseWeightCardCheck", false)
+        looseWeightEditor.apply()
+
+
+        val sharePrefBuildMuscle : SharedPreferences = context!!.getSharedPreferences(
+            "cardBuildMuscleChecked",
+            Context.MODE_PRIVATE
+        )
+        var buildMuscleEditor : SharedPreferences.Editor = sharePrefBuildMuscle.edit()
+        buildMuscleEditor.putBoolean("isBuildMuscleCardCheck", false)
+        buildMuscleEditor.apply()
+
+
+        val sharePrefBalance : SharedPreferences = context!!.getSharedPreferences(
+            "cardBalanceChecked",
+            Context.MODE_PRIVATE
+        )
+        var balanceEditor : SharedPreferences.Editor = sharePrefBalance.edit()
+        balanceEditor.putBoolean("isBalanceCardCheck", false)
+        balanceEditor.apply()
+    }
+
+    private fun resetDietType(){
+        val sharePrefVegDiet : SharedPreferences = context!!.getSharedPreferences(
+            "cardVegChecked",
+            Context.MODE_PRIVATE
+        )
+        var vegDietEditor : SharedPreferences.Editor = sharePrefVegDiet.edit()
+        vegDietEditor.putBoolean("isVegCardCheck", false)
+        vegDietEditor.apply()
+
+
+        val sharePrefCardNonVeg : SharedPreferences = context!!.getSharedPreferences(
+            "cardNonVegChecked",
+            Context.MODE_PRIVATE
+        )
+        var nonVegEditor : SharedPreferences.Editor = sharePrefCardNonVeg.edit()
+        nonVegEditor.putBoolean("isNonVegCardCheck", false)
+        nonVegEditor.apply()
+
+
+        val sharePrefMixedDiet : SharedPreferences = context!!.getSharedPreferences(
+            "cardMixedDietChecked",
+            Context.MODE_PRIVATE
+        )
+        var mixedDietEditor : SharedPreferences.Editor = sharePrefMixedDiet.edit()
+        mixedDietEditor.putBoolean("isMixDietCardCheck", false)
+        mixedDietEditor.apply()
     }
 }
