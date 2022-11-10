7ff84b5163e63d6adc2d40be087a9989cfb187ba
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2022-11-11 00:01:10 +0530

    [Code Changes]: Updated the diet list final. Updated the User profile Screen.

--- app/src/main/java/com/prathameshkumbhar/bfit/mainmodule/data/DataBaseOfDiet.kt
+++ app/src/main/java/com/prathameshkumbhar/bfit/mainmodule/data/DataBaseOfDiet.kt
@@ -1,17 +1,19 @@
 package com.prathameshkumbhar.bfit.mainmodule.data
 
+//Final List of the diet.
+
 var DIET_PLAN_LOOSE_WT_VEG_LIST = listOf(
     DietPlanDetails(
         1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Lukewarm Lemon Water/\n• 1 Glass Amla Juice"
     ),
     DietPlanDetails(
-        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 1 Plate Idli./\n• 1 Plate Oats."
+        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 1 Plate Idli. /\n• 1 Plate Oats."
     ),
     DietPlanDetails(
         1,"Lunch - 1:00 P.M.","https://i.imgur.com/ojPqrU4.jpg","• 2 Roti (oil free).\n• Sprouts Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
     ),
     DietPlanDetails(
-        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Cup Green Tea./\n• 1 Apple."
+        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Cup Green Tea. /\n• 1 Apple."
     ),
     DietPlanDetails(
         1,"Dinner - 9:00 P.M.","https://i.imgur.com/JFf2UyI.jpg","• 1 Bhakri (Nachni / Bajri / Jwari).\n• Green Vegetable (Spinach/Broccoli) etc."
@@ -19,16 +21,16 @@ var DIET_PLAN_LOOSE_WT_VEG_LIST = listOf(
 )
 var DIET_PLAN_LOOSE_WT_NON_VEG_LIST = listOf(
     DietPlanDetails(
-        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Lukewarm Lemon Water/\n• 1 Glass Amla Juice"
+        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Lukewarm Lemon Water /\n• 1 Glass Amla Juice"
     ),
     DietPlanDetails(
-        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/DKO9F8b.jpg","• 1 Brown Bread Egg Sandwich./\n• 1 Brown Bread Chicken Sandwich"
+        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/DKO9F8b.jpg","• 1 Brown Bread Egg Sandwich. /\n• 3 Boiled Egg (Whites)"
     ),
     DietPlanDetails(
         1,"Lunch - 1:00 P.M.","https://i.imgur.com/lRPEsTh.jpg","• 2 Roti (oil free).\n• Chicken Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
     ),
     DietPlanDetails(
-        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Cup Green Tea./\n• 1 Apple."
+        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Cup Green Tea. /\n• 1 Apple."
     ),
     DietPlanDetails(
         1,"Dinner - 9:00 P.M.","https://i.imgur.com/f1mZHS8.jpg","• 1 Bhakri (Nachni / Bajri / Jwari).\n• Fish / Egg Curry."
@@ -36,16 +38,16 @@ var DIET_PLAN_LOOSE_WT_NON_VEG_LIST = listOf(
 )
 var DIET_PLAN_LOOSE_WT_MIXED_LIST = listOf(
     DietPlanDetails(
-        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Lukewarm Lemon Water/\n• 1 Glass Amla Juice"
+        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Lukewarm Lemon Water /\n• 1 Glass Amla Juice"
     ),
     DietPlanDetails(
-        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 1 Brown Bread Egg Sandwich./\n• 1 Plate Oats."
+        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 1 Brown Bread Egg Sandwich. /\n• 1 Plate Oats."
     ),
     DietPlanDetails(
         1,"Lunch - 1:00 P.M.","https://i.imgur.com/lRPEsTh.jpg","• 2 Roti (oil free).\n• Sprouts / Chicken Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
     ),
     DietPlanDetails(
-        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Cup Green Tea./\n• 1 Apple."
+        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Cup Green Tea. /\n• 1 Apple."
     ),
     DietPlanDetails(
         1,"Dinner - 9:00 P.M.","https://i.imgur.com/JFf2UyI.jpg","• 1 Bhakri (Nachni / Bajri / Jwari).\n• Green Vegetable (Spinach/Broccoli) etc. / Egg Curry"
@@ -54,105 +56,104 @@ var DIET_PLAN_LOOSE_WT_MIXED_LIST = listOf(
 
 var DIET_PLAN_BUILD_MUSCLE_VEG_LIST = listOf(
     DietPlanDetails(
-        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Milk & Dry Fruits"
+        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/c8M0Gx5.jpg","• 1 Glass Milk & Dry Fruits"
     ),
     DietPlanDetails(
-        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 2 Roti (oil free).\n• Sprouts Curry."
+        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/9zi3pOf.jpg","• 2 Roti (oil free).\n• Sprouts Curry."
     ),
     DietPlanDetails(
-        1,"Lunch - 1:00 P.M.","https://i.imgur.com/ojPqrU4.jpg","• 2 Bhakri (Nachni / Bajri / Jwari).\n• Soybean Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
+        1,"Lunch - 1:00 P.M.","https://i.imgur.com/dq2GaYG.jpg","• 2 Bhakri (Nachni / Bajri / Jwari).\n• Soybean Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
     ),
     DietPlanDetails(
-        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Sandwich (Without Potato) /\n• 1 Paneer Roll."
+        1,"Evening - 5:00 P.M.","https://i.imgur.com/whk4oJx.jpg","• Fruit Salad /\n• 1 Sandwich (Without Potato)."
     ),
     DietPlanDetails(
-        1,"Dinner - 9:00 P.M.","https://i.imgur.com/JFf2UyI.jpg","• 2  Roti (oil free).\n• Paneer Curry."
+        1,"Dinner - 9:00 P.M.","https://i.imgur.com/ENEWDtd.jpg","• 2  Roti (oil free).\n• Paneer Curry."
     )
 )
 var DIET_PLAN_BUILD_MUSCLE_NON_VEG_LIST = listOf(
     DietPlanDetails(
-        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Milk & Dry Fruits"
+        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/c8M0Gx5.jpg","• 1 Glass Milk & Dry Fruits"
     ),
     DietPlanDetails(
-        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 2 Roti (oil free).\n• 1 Omelette."
+        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/e30cgYL.jpg","• 2 Roti (oil free).\n• 1 Omelette."
     ),
     DietPlanDetails(
-        1,"Lunch - 1:00 P.M.","https://i.imgur.com/ojPqrU4.jpg","• 2 Bhakri (Nachni / Bajri / Jwari).\n• Chicken Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
+        1,"Lunch - 1:00 P.M.","https://i.imgur.com/lRPEsTh.jpg","• 2 Bhakri (Nachni / Bajri / Jwari).\n• Chicken Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
     ),
     DietPlanDetails(
-        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Egg Roll / 1 Chicken Roll. \n• 3 Boiled Egg (Whites)"
+        1,"Evening - 5:00 P.M.","https://i.imgur.com/VxTYd50.jpg","• 1 Brown Bread Egg Sandwich / 1 Chicken Roll. \n• 3 Boiled Egg (Whites)"
     ),
     DietPlanDetails(
-        1,"Dinner - 9:00 P.M.","https://i.imgur.com/JFf2UyI.jpg","• 2 Roti (oil free).\n• Egg / Fish Curry."
+        1,"Dinner - 9:00 P.M.","https://i.imgur.com/f1mZHS8.jpg","• 2 Roti (oil free).\n• Egg Curry / Fish Curry."
     )
 )
 var DIET_PLAN_BUILD_MUSCLE_MIXED_LIST = listOf(
     DietPlanDetails(
-        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Milk & Dry Fruits"
+        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/c8M0Gx5.jpg","• 1 Glass Milk & Dry Fruits"
     ),
     DietPlanDetails(
-        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 2 Roti (oil free).\n• Sprouts Curry / Egg Curry .\n• 1 Bowl Rice & Dal. "
+        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/9zi3pOf.jpg","• 2 Roti (oil free).\n• Sprouts Curry / Egg Curry .\n• 1 Bowl Rice & Dal. "
     ),
     DietPlanDetails(
-        1,"Lunch - 1:00 P.M.","https://i.imgur.com/ojPqrU4.jpg","• 2 Bhakri (Nachni / Bajri / Jwari).\n• Soybean Curry / Chicken Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
+        1,"Lunch - 1:00 P.M.","https://i.imgur.com/lRPEsTh.jpg","• 2 Bhakri (Nachni / Bajri / Jwari).\n• Soybean Curry / Chicken Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
     ),
     DietPlanDetails(
-        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Paneer Roll / 1 Egg Roll\n• 3 Boiled Egg (Whites)"
+        1,"Evening - 5:00 P.M.","https://i.imgur.com/whk4oJx.jpg","• Fruit Salad\n• 3 Boiled Egg (Whites)"
     ),
     DietPlanDetails(
-        1,"Dinner - 9:00 P.M.","https://i.imgur.com/JFf2UyI.jpg","• 2 Roti (oil free).\n• Paneer Curry / Fish Curry."
+        1,"Dinner - 9:00 P.M.","https://i.imgur.com/ENEWDtd.jpg","• 2 Roti (oil free).\n• Paneer Curry / Fish Curry."
     )
 )
 
-//Add diet from below
 var DIET_PLAN_BALANCED_VEG_LIST = listOf(
     DietPlanDetails(
-        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","•  1 Glass Lukewarm Lemon Water/\n•  1 Glass Amla Juice"
+        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","•  1 Glass Lukewarm Lemon Water /\n•  1 Glass Amla Juice"
     ),
     DietPlanDetails(
-        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 1 Plate Idli./\n• 1 Plate Oats."
+        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/ENEWDtd.jpg","• 2 Roti (oil free).\n• Paneer Curry."
     ),
     DietPlanDetails(
-        1,"Lunch - 1:00 P.M.","https://i.imgur.com/ojPqrU4.jpg","• 2 Roti (oil free).\n• Sprouts Curry / Paneer Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
+        1,"Lunch - 1:00 P.M.","https://i.imgur.com/ojPqrU4.jpg","• 2 Roti (oil free).\n• Sprouts Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
     ),
     DietPlanDetails(
-        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Cup Green Tea./\n• 1 Apple."
+        1,"Evening - 5:00 P.M.","https://i.imgur.com/whk4oJx.jpg","• Fruit Salad. /\n• 2 Apple."
     ),
     DietPlanDetails(
-        1,"Dinner - 9:00 P.M.","https://i.imgur.com/JFf2UyI.jpg","• 1 Bhakri (Nachni / Bajri / Jwari).\n• Green Vegetable (Spinach/Broccoli) etc."
+        1,"Dinner - 9:00 P.M.","https://i.imgur.com/dq2GaYG.jpg","• 1 Bhakri (Nachni / Bajri / Jwari).\n• Green Vegetable (Spinach/Broccoli) etc. / Soybean Curry."
     )
 )
 var DIET_PLAN_BALANCED_NON_VEG_LIST = listOf(
     DietPlanDetails(
-        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Lukewarm Lemon Water/\n• 1 Glass Amla Juice"
+        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Lukewarm Lemon Water /\n• 1 Glass Amla Juice"
     ),
     DietPlanDetails(
-        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 1 Plate Idli./\n• 1 Plate Oats."
+        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/e30cgYL.jpg","• 2 Roti (oil free).\n• 1 Omelette."
     ),
     DietPlanDetails(
-        1,"Lunch - 1:00 P.M.","https://i.imgur.com/ojPqrU4.jpg","• 2 Roti (oil free).\n• Sprouts Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
+        1,"Lunch - 1:00 P.M.","https://i.imgur.com/lRPEsTh.jpg","• 2 Roti (oil free).\n• Chicken Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
     ),
     DietPlanDetails(
-        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Cup Green Tea./\n• 1 Apple."
+        1,"Evening - 5:00 P.M.","https://i.imgur.com/DKO9F8b.jpg","• 1 Brown Bread Egg Sandwich. /\n• 1 Chicken Roll."
     ),
     DietPlanDetails(
-        1,"Dinner - 9:00 P.M.","https://i.imgur.com/JFf2UyI.jpg","• 1 Bhakri (Nachni / Bajri / Jwari).\n• Green Vegetable (Spinach/Broccoli) etc."
+        1,"Dinner - 9:00 P.M.","https://i.imgur.com/f1mZHS8.jpg","• 1 Bhakri (Nachni / Bajri / Jwari).\n• Egg Curry / Fish Curry."
     )
 )
 var DIET_PLAN_BALANCED_MIXED_LIST = listOf(
     DietPlanDetails(
-        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Lukewarm Lemon Water/\n• 1 Glass Amla Juice"
+        1,"Early Morning - 6:00 A.M.","https://i.imgur.com/VIOjOVF.jpg","• 1 Glass Lukewarm Lemon Water /\n• 1 Glass Amla Juice"
     ),
     DietPlanDetails(
-        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/pxyEnPY.jpg","• 1 Plate Idli./\n• 1 Plate Oats."
+        1,"Breakfast - 9:00 A.M.","https://i.imgur.com/e30cgYL.jpg","• 2 Roti (oil free).\n• Paneer Curry / 1 Omelette."
     ),
     DietPlanDetails(
-        1,"Lunch - 1:00 P.M.","https://i.imgur.com/ojPqrU4.jpg","• 2 Roti (oil free).\n• Sprouts Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
+        1,"Lunch - 1:00 P.M.","https://i.imgur.com/lRPEsTh.jpg","• 2 Roti (oil free).\n• Sprouts Curry / Chicken Curry + Veg Salad.\n• 1 Bowl Rice & Dal."
     ),
     DietPlanDetails(
-        1,"Evening - 5:00 P.M.","https://i.imgur.com/YKk9BFe.jpg","• 1 Cup Green Tea./\n• 1 Apple."
+        1,"Evening - 5:00 P.M.","https://i.imgur.com/whk4oJx.jpg","• Fruit Salad. /\n• 1 Brown Bread Egg Sandwich."
     ),
     DietPlanDetails(
-        1,"Dinner - 9:00 P.M.","https://i.imgur.com/JFf2UyI.jpg","• 1 Bhakri (Nachni / Bajri / Jwari).\n• Green Vegetable (Spinach/Broccoli) etc."
+        1,"Dinner - 9:00 P.M.","https://i.imgur.com/dq2GaYG.jpg","• 1 Bhakri (Nachni / Bajri / Jwari).\n• Soybean curry / Fish Curry."
     )
 )
